3888b61aa809d43299542877ce5d78ba11e1eb70
VasileiosDiamantidis <vasileios.diamantidis@zattoo.com>
2022-03-16 10:05:35 +0100

    Initial commit adds basic code to run a stream

---
+++ FastForwardIssue_seek_to_startUITests/FastForwardIssue_seek_to_startUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FastForwardIssue_seek_to_startUITestsLaunchTests.swift
+//  FastForwardIssue_seek_to_startUITests
+//
+//  Created by Vasileios on 15.03.22.
+//
+
+import XCTest
+
+class FastForwardIssue_seek_to_startUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
